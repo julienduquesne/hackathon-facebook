9d8f4665221104957984c50a2b50a6081f6eaae4
François Remili <francois.remili@mines-paristech.fr>
2019-03-30 20:23:36 +0100

    debugged json_parser.py

--- python/json_parser.py
+++ python/json_parser.py
@@ -11,13 +11,20 @@ def parse_conversation(conv):
     return messages_list
 
 
+def parse_conversation_from_file(path):
+    with open(path, encoding="utf8") as json_file:
+        conversation = json.load(json_file)
+        messages_list = []
+        for json_message in conversation:
+            if json_message["type"] == 'message':
+                messages_list.append(parse_message(json_message))
+    return messages_list
+
+
 def parse_message(m):
     message = Message(author=m["senderID"], body=m["body"],
                       timestamp=m["timestamp"], reactions=m["messageReactions"])
     return message
-
-
-
 
 
 
