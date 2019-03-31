f4629c950fbda470440899704fb595100c8a0b02
François Remili <francois.remili@mines-paristech.fr>
2019-03-31 03:10:48 +0200

    added attachments feature

--- python/message.py
+++ python/message.py
@@ -8,10 +8,11 @@ class Message:
     timestamp
     '''
 
-    def __init__(self, author, body, timestamp, reactions):
+    def __init__(self, author, body, timestamp, reactions, attachments):
         self.author = author
         self.body = body
         self.timestamp = timestamp
         self.reactions = reactions
+        self.attachments = attachments
 
 
